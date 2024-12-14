9746404a6eb49e5055ad3f10c4ced08069c65c3f
ashraful2871 <ashrafulislam2758@gmail.com>
2024-12-14 13:51:35 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBH6Q3Ox5mZuo5Pcusd4skzckkdh0rDhg0",
+  authDomain: "sholo-sharp.firebaseapp.com",
+  projectId: "sholo-sharp",
+  storageBucket: "sholo-sharp.firebasestorage.app",
+  messagingSenderId: "86559318538",
+  appId: "1:86559318538:web:71fea903abd0f0004c35f3",
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
